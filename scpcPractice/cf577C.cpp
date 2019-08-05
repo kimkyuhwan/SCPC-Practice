25a0fd581e1b61f90ead77cac490c35123b362bd
김규환 <gmarble94@gmail.com>
2019-08-05 12:51:20 +0900

    codeforce C modify

--- scpcPractice/cf577C.cpp
+++ scpcPractice/cf577C.cpp
@@ -1,41 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-map<int, int> aa;
-vector<int> vec;
-int n, k, ans;
-vector<int> num;
+map<ll, ll> aa;
+vector<ll> vec;
+ll n, k;
+ll ans;
+vector<ll> num;
+vector<ll> nextCnt;
 int main()
 {
-	scanf("%d %d", &n, &k);
+	scanf("%lld %lld", &n, &k);
 	vec.resize(n);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &vec[i]);
+		scanf("%lld", &vec[i]);
 	}
 	sort(vec.begin(), vec.end());
 	for (int i = n / 2; i < n; i++) {
 		aa[vec[i]]++;
 	}
+	int ccnt = 0;
+	nextCnt.resize(aa.size());
 	for (auto it = aa.begin(); it != aa.end(); it++) {
 		num.push_back(it->first);
+		if (ccnt > 0) {
+			nextCnt[ccnt] = nextCnt[ccnt - 1];
+		}
+		nextCnt[ccnt] += it->second;
+		ccnt++;
 	}
-	ans = num[0];
-	for (int i = 0; i < num.size()-1; i++) {
-		int nextDifference = num[i + 1] - num[i];
-		int nextCnt = aa[num[i]];
-		ll cost = 1LL * nextCnt * nextDifference;
+	ans = -1;
+	for (int i = 0; i < num.size() - 1; i++) {
+		ll nextDifference = num[i + 1] - num[i];
+		ll cost = 1LL * nextCnt[i] * nextDifference;
 		if (cost > k) {
-			int diff = k / nextCnt;
+			ll diff = k / nextCnt[i];
 			ans = max(ans, num[i] + diff);
-			k = 0;
+			break;
 		}
 		else {
-			ans = max(ans, num[i + 1]);
 			k -= cost;
 		}
 	}
-	if (k != 0) {
-		ans += k / ((n / 2)+1);
+	if (ans == -1) {
+		int l = nextCnt.size() - 1;
+		ans = num[l] + k / (nextCnt[l]);
 	}
-	printf("%d\n", ans);
+	printf("%lld\n", ans);
 }
